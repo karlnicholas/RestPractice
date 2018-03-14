69b9c7e6d300bc9d9b226e95d52e1b2f973c4c60
Karl Nicholas <karl.nicholas@outlook.com>
2018-03-14 13:14:12 -0700

    Start Zuul LoadBalancing Server.

---
+++ EmployeeZuul/src/main/java/employeezuul/EmployeeZuulApplication.java
@@ -0,0 +1,12 @@
+package employeezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeZuulApplication.class, args);
+	}
+}
