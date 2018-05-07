2d94868e35dda5163bb3336b761440c11de32af6
Karl Nicholas <karl.nicholas@outlook.com>
2018-05-07 14:49:18 -0700

    Formatting.

--- EmployeeProject/src/main/java/employeeproject/service/ProjectRepository.java
+++ EmployeeProject/src/main/java/employeeproject/service/ProjectRepository.java
@@ -11,8 +11,8 @@ import org.springframework.data.repository.query.Param;
 import employeeproject.model.Project;
 import project.item.ProjectItem;
 
-    public interface ProjectRepository extends JpaRepository<Project, Integer> {
-        public Page<ProjectItem> findAllBy(Pageable pageRequest);
-        @Query("select new project.item.ProjectItem(p.projectId, p.projectName, p.techstack) from Project p where p.projectId in (select ep.employeeProjectId.projectId from EmployeeProject ep where ep.employeeProjectId.empId = :empId)")
-        public List<ProjectItem> findAllByEmployeeProjectEmpId(@Param("empId") Integer empId);
-    }
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    public Page<ProjectItem> findAllBy(Pageable pageRequest);
+    @Query("select new project.item.ProjectItem(p.projectId, p.projectName, p.techstack) from Project p where p.projectId in (select ep.employeeProjectId.projectId from EmployeeProject ep where ep.employeeProjectId.empId = :empId)")
+    public List<ProjectItem> findAllByEmployeeProjectEmpId(@Param("empId") Integer empId);
+}
