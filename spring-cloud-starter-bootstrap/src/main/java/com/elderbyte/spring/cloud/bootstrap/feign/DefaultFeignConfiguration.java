8da3d568d65b120c9d8e282b62e6863324ff7b17
Karl Nicholas <karl.nicholas@outlook.com>
2018-05-23 17:01:16 -0700

    Fix some build issues.

--- spring-cloud-starter-bootstrap/src/main/java/com/elderbyte/spring/cloud/bootstrap/feign/DefaultFeignConfiguration.java
+++ spring-cloud-starter-bootstrap/src/main/java/com/elderbyte/spring/cloud/bootstrap/feign/DefaultFeignConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 
 /**
--- spring-cloud-starter-bootstrap/src/main/java/com/elderbyte/spring/cloud/bootstrap/feign/PageableQueryEncoder.java
+++ spring-cloud-starter-bootstrap/src/main/java/com/elderbyte/spring/cloud/bootstrap/feign/PageableQueryEncoder.java
@@ -11,44 +11,44 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-    /**
-     * This encoder adds support for pageable, which will be applied to the query parameters.
-     */
-    public class PageableQueryEncoder implements Encoder {
+/**
+ * This encoder adds support for pageable, which will be applied to the query parameters.
+ */
+public class PageableQueryEncoder implements Encoder {
 
-        private final Encoder delegate;
+    private final Encoder delegate;
 
-        public PageableQueryEncoder(Encoder delegate){
-            this.delegate = delegate;
-        }
+    public PageableQueryEncoder(Encoder delegate){
+        this.delegate = delegate;
+    }
 
-        @Override
-        public void encode(Object object, Type bodyType, RequestTemplate template) throws EncodeException {
+    @Override
+    public void encode(Object object, Type bodyType, RequestTemplate template) throws EncodeException {
 
-            if(object instanceof Pageable) {
-                Pageable pageable = (Pageable) object;
-                template.query("page", pageable.getPageNumber() + "");
-                template.query("size", pageable.getPageSize() + "");
-                if (pageable.getSort() != null) {
-                    applySort(template, pageable.getSort());
-                }
-            }else if(object instanceof Sort){
-                Sort sort = (Sort)object;
-                applySort(template, sort);
-            }else{
-                delegate.encode(object, bodyType, template);
+        if(object instanceof Pageable) {
+            Pageable pageable = (Pageable) object;
+            template.query("page", pageable.getPageNumber() + "");
+            template.query("size", pageable.getPageSize() + "");
+            if (pageable.getSort() != null) {
+                applySort(template, pageable.getSort());
             }
-        }
-
-
-        private void applySort(RequestTemplate template, Sort sort){
-            Collection<String> existingSorts = template.queries().get("sort");
-            List<String> sortQueries  = existingSorts != null ? new ArrayList<>(existingSorts) : new ArrayList<>();
-            for (Sort.Order order : sort) {
-                sortQueries.add(order.getProperty() + "," + order.getDirection());
-            }
-            if(!sortQueries.isEmpty()){
-                template.query("sort", sortQueries);
-            }
+        }else if(object instanceof Sort){
+            Sort sort = (Sort)object;
+            applySort(template, sort);
+        }else{
+            delegate.encode(object, bodyType, template);
         }
     }
+
+
+    private void applySort(RequestTemplate template, Sort sort){
+        Collection<String> existingSorts = template.queries().get("sort");
+        List<String> sortQueries  = existingSorts != null ? new ArrayList<>(existingSorts) : new ArrayList<>();
+        for (Sort.Order order : sort) {
+            sortQueries.add(order.getProperty() + "," + order.getDirection());
+        }
+        if(!sortQueries.isEmpty()){
+            template.query("sort", sortQueries);
+        }
+    }
+}
